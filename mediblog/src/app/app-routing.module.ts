c21b056abb23ce17003d9eca027cfea9e67e6241
RajaduraiPal <rajadurai282@gmail.com>
2019-01-27 18:02:03 +0530

    Components Added
    Components Added

--- mediblog/src/app/app-routing.module.ts
+++ mediblog/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PostdetailComponent } from './postdetail/postdetail.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'post Detail', component: PostdetailComponent },
+  { path: 'Home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mediblog/src/app/app.module.ts
+++ mediblog/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PostdetailComponent } from './postdetail/postdetail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostdetailComponent
+    PostdetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
